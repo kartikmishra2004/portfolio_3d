6e25482e98225c150f55da60e94e6bd3cf6f653c
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-05-21 23:38:22 +0530

    Refactor #2

--- src/app/_components/Home/Hero.tsx
+++ src/app/_components/Home/Hero.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { AuroraBackground } from "@/components/ui/aurora-background";
 import { Button } from "@/components/ui/button";
 import { FlipWords } from "@/components/ui/flip-words";
 import Link from "next/link";
@@ -54,39 +53,37 @@ export default function Hero() {
     });
 
     return (
-        <section className="w-full h-screen">
-            <AuroraBackground>
-                <div className="w-full z-0 h-screen flex flex-col gap-8 px-40 justify-center">
-                    <div>
-                        <div className="overflow-hidden">
-                            <h1 ref={titleRef} className="text-9xl cursor-effect font-extrabold text-foreground/80 w-max tracking-tighter">KARTIK MISHRA</h1>
-                        </div>
-                        <div className="overflow-hidden">
-                            <div ref={fliptextRef} className="text-3xl mb-3">
-                                <FlipWords words={words} />
-                            </div>
-                        </div>
-                        <div className="overflow-hidden">
-                            <p ref={description1Ref} className="text-muted-foreground/70 mt-6 text-sm tracking-widest w-1/2 font-semibold">Turning ideas into interactive web experiences. Clean code. Sleek</p>
-                        </div>
-                        <div className="overflow-hidden">
-                            <p ref={description2Ref} className="text-muted-foreground/70 text-sm tracking-widest w-1/2 font-semibold">design. Seamless performance.</p>
+        <section>
+            <div className="w-full z-0 h-screen flex bg-gradient-to-br from-background via-accent to-background flex-col gap-8 px-40 justify-center">
+                <div>
+                    <div className="overflow-hidden">
+                        <h1 ref={titleRef} className="text-9xl cursor-effect font-extrabold text-foreground/80 w-max tracking-tighter">KARTIK MISHRA</h1>
+                    </div>
+                    <div className="overflow-hidden">
+                        <div ref={fliptextRef} className="text-3xl mb-3">
+                            <FlipWords words={words} />
                         </div>
                     </div>
-                    <div ref={buttonRef} className="w-full flex gap-2">
-                        <Link href={'/projects'}>
-                            <Button size={"lg"} className="cursor-pointer">
-                                View My Work
-                            </Button>
-                        </Link>
-                        <Link href={'/contact'}>
-                            <Button variant={'secondary'} size={"lg"} className="cursor-pointer">
-                                Get In Touch
-                            </Button>
-                        </Link>
+                    <div className="overflow-hidden">
+                        <p ref={description1Ref} className="text-muted-foreground/70 mt-6 text-sm tracking-widest w-1/2 font-semibold">Turning ideas into interactive web experiences. Clean code. Sleek</p>
+                    </div>
+                    <div className="overflow-hidden">
+                        <p ref={description2Ref} className="text-muted-foreground/70 text-sm tracking-wider w-1/2 font-semibold">design. Seamless performance.</p>
                     </div>
                 </div>
-            </AuroraBackground>
+                <div ref={buttonRef} className="w-full flex gap-2">
+                    <Link href={'/projects'}>
+                        <Button size={"lg"} className="cursor-pointer">
+                            View My Work
+                        </Button>
+                    </Link>
+                    <Link href={'/contact'}>
+                        <Button variant={'outline'} size={"lg"} className="cursor-pointer">
+                            Get In Touch
+                        </Button>
+                    </Link>
+                </div>
+            </div>
         </section>
     )
 }
